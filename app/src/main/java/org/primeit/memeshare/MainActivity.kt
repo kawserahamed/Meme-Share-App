a1be85344f1d354a45fb1f271aa8642b678e2574
ahamed404 <kawserarasel24@gmail.com>
2022-04-13 12:28:30 +0600

    Share Button Added using Singleton pattern

--- app/src/main/java/org/primeit/memeshare/MainActivity.kt
+++ app/src/main/java/org/primeit/memeshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.primeit.memeshare
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
@@ -7,7 +8,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -17,6 +17,7 @@ import org.primeit.memeshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    var currentImageUrl: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,16 +32,16 @@ class MainActivity : AppCompatActivity() {
 
         binding.progressBarId.visibility = View.VISIBLE
         // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
+
         val url = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             { response ->
-                val memeUrl = response.getString("url")
+                currentImageUrl = response.getString("url")
 
-                Glide.with(this).load(memeUrl).listener(object : RequestListener<Drawable> {
+                Glide.with(this).load(currentImageUrl).listener(object : RequestListener<Drawable> {
 
                     override fun onLoadFailed(
                         e: GlideException?,
@@ -65,19 +66,26 @@ class MainActivity : AppCompatActivity() {
 
                 }).into(binding.imMeme)
 
-
             },
             {
                 Toast.makeText(this, "Something is wrong", Toast.LENGTH_SHORT).show()
             })
 
         // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
 
     }
 
     fun share(view: View) {
 
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Hey, Checkout this cool meme I got from Reddit $currentImageUrl"
+        )
+        val chooser = Intent.createChooser(intent, "share this meme using ...")
+        startActivity(intent)
 
     }
 
